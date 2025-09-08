318eca7fe002af1dcb9319605bcc667a96f995e3
Omer Shireen <omer7sce@gmail.com>
2025-09-09 01:22:45 +0500

    Point class

---
+++ Point/Point.py
@@ -0,0 +1,19 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x 
+        self.y = y
+
+    def __sub__(self,other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __str__(self):
+        return f"Point :{self.x} , {self.y} "
+
+
+p1 = Point(55,60)
+p2 = Point(15,20)
+p3 = p1 - p2 
+
+print(p1)
+print(p2)
+print(p3)
