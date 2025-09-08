4d9e31b70dbdd56d9a5d366d43253d621d4a01ed
Omer Shireen <omer7sce@gmail.com>
2025-09-09 01:26:40 +0500

    updated code

--- Point/Point.py
+++ Point/Point.py
@@ -3,8 +3,8 @@ class Point:
         self.x = x 
         self.y = y
 
-    def __sub__(self,other):
-        return Point(self.x - other.x, self.y - other.y)
+    def __add__(self,other):
+        return Point(self.x + other.x, self.y + other.y)
 
     def __str__(self):
         return f"Point :{self.x} , {self.y} "
@@ -12,7 +12,7 @@ class Point:
 
 p1 = Point(55,60)
 p2 = Point(15,20)
-p3 = p1 - p2 
+p3 = p1 + p2 
 
 print(p1)
 print(p2)
