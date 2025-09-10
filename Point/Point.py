f8419699eafa7298896b4451a49ccf0b499039dc
Omer Shireen <omer7sce@gmail.com>
2025-09-10 15:50:38 +0500

    main file added

--- Point/Point.py
+++ Point/Point.py
@@ -10,10 +10,3 @@ class Point:
         return f"Point :{self.x} , {self.y} "
 
 
-p1 = Point(55,60)
-p2 = Point(15,20)
-p3 = p1 + p2 
-
-print(p1)
-print(p2)
-print(p3)
---
+++ Point/main.py
@@ -0,0 +1,17 @@
+from Point import Point
+def main():
+
+     p1 = Point(55,60)
+     p2 = Point(15,20)
+     p3 = p1 + p2 
+
+     print(p1)
+     print(p2)
+     print(p3)
+
+
+
+if __name__=="__main__":
+    main()
+
+  
