c3854e07fb24ae29b4cfc0a12922f343a6da8eea
Omer Shireen <omer7sce@gmail.com>
2025-09-12 20:27:14 +0500

    Rational class updated

--- Rational/Rational.py
+++ Rational/Rational.py
@@ -1,3 +1,20 @@
 class Rational:
     def __init__(self, numerator:int , denominator:int):
-        
+        if denominator == 0:
+            raise ValueError("Denominator cannot be zero")
+
+        if denominator < 0:
+            numerator , denominator = -numerator , -denominator
+
+        divisor = gcd(numerator,denominator)
+        self.__numerator = numerator // divisor
+        self.__denominator = denominator // divisor
+
+@property
+def numerator(self):
+    return self.__numerator
+
+@property
+def denominator(self):
+    return self.__denominator                 
+
