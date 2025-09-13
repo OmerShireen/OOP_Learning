c5dc94be62abf817ec89a42a3511ee1bab2611e1
Omer Shireen <omer7sce@gmail.com>
2025-09-13 16:10:49 +0500

    Multiplication function added

--- Rational/Rational.py
+++ Rational/Rational.py
@@ -18,6 +18,7 @@ def numerator(self):
 def denominator(self):
     return self.__denominator      
 
+
 def __add__(self, other):
     if not isinstance(other, Rational):
         return NotImplemented
@@ -31,5 +32,15 @@ def __sub__(self, other):
         return NotImplemented
     num = self.numerator * other.denominator - other.numerator * self.denominator
     den = self.denominator * other.denominator 
-    return Rational(num, den)             
+    return Rational(num, den)  
+
+
+def __mul__(self, other):
+    if not isinstance(other, Rational):
+        return NotImplemented
+    num = self.numerator * other.numerator 
+    den = self.denominator * other.denominator
+    return Rational(num, den)
+        
+
 
