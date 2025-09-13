45e0d813ff152eba18a0f18920aa72d3e0851723
Omer Shireen <omer7sce@gmail.com>
2025-09-13 16:00:54 +0500

    Add function added

--- Rational/Rational.py
+++ Rational/Rational.py
@@ -16,5 +16,12 @@ def numerator(self):
 
 @property
 def denominator(self):
-    return self.__denominator                 
+    return self.__denominator      
+
+def __add__(self, other):
+    if not isinstance(other, Rational):
+        return NotImplemented
+    num = self.numerator * other.denominator + other.numerator * self.denominator
+    den = self.denominator * other.denominator
+    return Rational(num, den)                   
 
