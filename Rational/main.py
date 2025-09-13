8126305d45cf2241d1ee4e8edf7261d4fd45fe8b
Omer Shireen <omer7sce@gmail.com>
2025-09-13 16:54:22 +0500

    Main file added

---
+++ Rational/main.py
@@ -0,0 +1,24 @@
+from Rational import Rational
+def main():
+
+ r1 = Rational(22, 44)   
+ r2 = Rational(14, 28)   
+
+ print("r1:", r1)  
+ print("r2:", r2)  
+
+ print("r1 + r2 =", r1 + r2)  
+ print("r1 - r2 =", r1 - r2)  
+ print("r1 * r2 =", r1 * r2)  
+ print("r1 / r2 =", r1 / r2)  
+
+ print("float(r1):", float(r1)) 
+ print("r1 == r2:", r1 == r2)   
+
+if __name__=="__main__":
+    main()
+
+
+
+
+
