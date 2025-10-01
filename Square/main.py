b55f66c18f936b244506e13d360c44049495362e
Omer Shireen <omer7sce@gmail.com>
2025-10-01 19:15:33 +0500

    Main file

--- Square/main.py
+++ Square/main.py
@@ -1,2 +1,14 @@
 from Square import SquareCalculator
 def main():
+ sq1 = SquareCalculator(20)
+ sq1.traditional()
+ sq1.display()
+
+ sq2 = SquareCalculator(30)
+ sq2.pythonic()
+ sq2.display()
+ 
+
+
+if __name__=="__main__":
+    main()
